bf699616fc246495696e22159185c6e3bb8ded11
Jakub Holak <jakub.holak@nordcloud.com>
2019-02-20 16:24:50 +0100

    Add hosted ui

---
+++ hostedui/src/index.js
@@ -0,0 +1,81 @@
+import Amplify, { Auth, Hub, Logger } from "aws-amplify";
+
+const jsonConfig = window.amplifyCognitoConfig;
+const socketUrl = window.socket;
+
+const oauth = {
+  domain: jsonConfig.domain,
+  scope: ["email"],
+  redirectSignIn: jsonConfig.redirectSignIn,
+  redirectSignOut: jsonConfig.redirectSignOut,
+  responseType: "code",
+  options: {
+    AdvancedSecurityDataCollectionFlag: true
+  }
+};
+
+export const amplifyConfig = {
+  Auth: {
+    identityPoolId: jsonConfig.identityPoolId,
+    region: jsonConfig.region,
+    userPoolId: jsonConfig.userPoolId,
+    userPoolWebClientId: jsonConfig.userPoolWebClientId,
+    mandatorySignIn: true,
+    oauth: oauth
+  }
+};
+
+Auth.configure(amplifyConfig);
+
+const { domain, redirectSignIn, redirectSignOut, responseType } = oauth;
+
+const sendToSocket = message => {
+  const socketConnection = new WebSocket(socketUrl, "echo-protocol");
+  socketConnection.onopen = function(event) {
+    socketConnection.send(message);
+    socketConnection.close();
+    window.close();
+  };
+};
+
+const OAuthSignIn = () =>
+  window.location.assign(
+    "https://" +
+      domain +
+      "/oauth2/authorize?redirect_uri=" +
+      redirectSignIn +
+      "&response_type=" +
+      responseType +
+      "&client_id=" +
+      jsonConfig.userPoolWebClientId +
+      "&identity_provider=Google"
+  );
+
+const checkCurrentUser = () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const hasAuthCode = urlParams.get("code");
+  Auth.currentAuthenticatedUser()
+    .then(user => sendToSocket(user.signInUserSession.idToken.jwtToken))
+    .catch(e => {
+      if (hasAuthCode) {
+        const alex = new Logger("Alexander_the_auth_watcher");
+        alex.onHubCapsule = capsule => {
+          switch (capsule.payload.event) {
+            case "signIn":
+              checkCurrentUser();
+              break;
+            case "configured":
+              alex.error("the Auth module is configured");
+              break;
+            default:
+              break;
+          }
+        };
+        Hub.listen("auth", alex);
+      } else {
+        OAuthSignIn();
+      }
+    });
+};
+
+checkCurrentUser();
---
+++ src/cognito/hosteduibased.ts
@@ -0,0 +1,26 @@
+import * as fs from "fs";
+const express = require("express");
+const expressWs = require("express-ws");
+const opn = require("opn");
+
+const getTokenFromHostedUI = (setup: any): Promise<string> =>
+  new Promise(resolve => {
+    const config = fs.readFileSync(setup.hostedUI).toString();
+    const app = express();
+    expressWs(app);
+    app.use(express.static("hostedui/dist"));
+    app.get("/", (req: any, res: any) =>
+      res.send(
+        `<!DOCTYPE html><html><head><script>var amplifyCognitoConfig = ${config};var socket = "ws://localhost:3000";</script></head><body><div id="app"></div><script src="./cognitohosteduilauncher.js"></script></body></html>`
+      )
+    );
+    app.ws("/", function(ws: any) {
+      ws.on("message", function(msg: any) {
+        resolve(msg);
+      });
+    });
+    app.listen(3000);
+    opn("http://localhost:3000");
+  });
+
+export { getTokenFromHostedUI };
