888e8c8105bc3e0e355a09118ddce012b2f418d4
Jakub Holak <jakub.holak@nordcloud.com>
2019-06-14 11:37:12 +0200

    Fix path to js

--- src/cognito/hosteduibased.ts
+++ src/cognito/hosteduibased.ts
@@ -3,15 +3,19 @@ const express = require("express");
 const expressWs = require("express-ws");
 const opn = require("opn");
 
+const pathToJs = __filename.split("/");
+pathToJs.splice(-3, 3);
+console.log(pathToJs.join("/") + "hostedui/dist");
 const getTokenFromHostedUI = (setup: any): Promise<string> =>
   new Promise(resolve => {
     const config = fs.readFileSync(setup.hostedUI).toString();
     const app = express();
     expressWs(app);
-    app.use(express.static("hostedui/dist"));
+    app.use(express.static(pathToJs.join("/") + "/hostedui/dist"));
     app.get("/", (req: any, res: any) =>
       res.send(
-        `<!DOCTYPE html><html><head><script>var amplifyCognitoConfig = ${config};var socket = "ws://localhost:3000";</script></head><body><div id="app"></div><script src="./cognitohosteduilauncher.js"></script></body></html>`
+        `<!DOCTYPE html><html><head><!-- ${pathToJs.join("/") +
+          "/hostedui/dist"} --><script>var amplifyCognitoConfig = ${config};var socket = "ws://localhost:3000";</script></head><body><div id="app"></div><script src="./cognitohosteduilauncher.js"></script></body></html>`
       )
     );
     app.ws("/", function(ws: any) {
